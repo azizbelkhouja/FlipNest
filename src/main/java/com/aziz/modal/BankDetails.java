fbb65a1258e1422e54634d58411bd5cf79ea0d8b
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-10-18 22:58:25 +0200

    BankDetails class

--- src/main/java/com/aziz/modal/BankDetails.java
+++ src/main/java/com/aziz/modal/BankDetails.java
@@ -1,4 +1,10 @@
 package com.aziz.modal;
 
+import lombok.Data;
+
+@Data
 public class BankDetails {
+    private String accountNumber;
+    private String accountHolderName;
+    private String iban;
 }
