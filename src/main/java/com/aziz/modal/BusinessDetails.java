de7e487e6b0c6cd85ab7ff5ce2600e8ed992f0e0
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-10-18 22:59:43 +0200

    BusinessDetails class

--- src/main/java/com/aziz/modal/BusinessDetails.java
+++ src/main/java/com/aziz/modal/BusinessDetails.java
@@ -1,4 +1,13 @@
 package com.aziz.modal;
 
+import lombok.Data;
+
+@Data
 public class BusinessDetails {
+    private String businessName;
+    private String businessAddress;
+    private String businessMobile;
+    private String businessEmail;
+    private String logo;
+    private String banner;
 }
