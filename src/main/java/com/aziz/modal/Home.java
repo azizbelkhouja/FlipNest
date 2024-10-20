52646a0e85e921148755da36255cad9777cff649
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-10-20 15:35:23 +0200

    Home class

---
+++ src/main/java/com/aziz/modal/Home.java
@@ -0,0 +1,11 @@
+package com.aziz.modal;
+
+import java.util.List;
+
+public class Home {
+
+    private List<HomeCategory> grid;
+    private List<HomeCategory> shopByCategory;
+    private List<HomeCategory> electronicCategories;
+    private List<HomeCategory> dealsCategories;
+}
