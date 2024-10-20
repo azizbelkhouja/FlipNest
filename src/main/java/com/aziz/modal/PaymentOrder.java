88d08c6917aea42987331b67538d72a7f755d1ec
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-10-20 15:21:44 +0200

    PaymentOrder class

--- src/main/java/com/aziz/modal/PaymentOrder.java
+++ src/main/java/com/aziz/modal/PaymentOrder.java
@@ -3,7 +3,11 @@ package com.aziz.modal;
 import com.aziz.domain.PaymentMethod;
 import com.aziz.domain.PaymentOrderStatus;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -27,4 +31,7 @@ public class PaymentOrder {
 
     @ManyToOne
     private User user;
+
+     @OneToMany
+    private Set<Order> orders = new HashSet<>();
 }
