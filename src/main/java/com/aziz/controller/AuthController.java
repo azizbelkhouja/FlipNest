bd8e380658f06bc954b424fac826a567f52ed33d
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-10-20 23:12:28 +0200

    firstusercreated

--- src/main/java/com/aziz/controller/AuthController.java
+++ src/main/java/com/aziz/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.aziz.controller;
 
 import com.aziz.modal.User;
+import com.aziz.repository.UserRepository;
 import com.aziz.response.SignupRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final UserRepository userRepository;
 
     @PostMapping("/signup")
     public ResponseEntity<User> createUserHandler(@RequestBody SignupRequest req) {
@@ -18,6 +23,9 @@ public class AuthController {
         User user = new User();
         user.setEmail(req.getEmail());
         user.setFullName(req.getFullName());
-        return ResponseEntity.ok(user);
+
+        User savedUser = userRepository.save(user);
+
+        return ResponseEntity.ok(savedUser);
     }
 }
--- src/main/java/com/aziz/repository/UserRepository.java
+++ src/main/java/com/aziz/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package com.aziz.repository;
 
+import com.aziz.modal.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User,Long> {
 
 
 }
