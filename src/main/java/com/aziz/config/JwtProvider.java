840b9dc7b42388c0a9ddbf3cbc1a833105b66182
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-10-22 22:35:40 +0200

    started provider

---
+++ src/main/java/com/aziz/config/JwtProvider.java
@@ -0,0 +1,11 @@
+package com.aziz.config;
+
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+
+public class JwtProvider {
+
+    SecretKey key = Keys.hmacShaKeyFor(JWT_CONSTANT.SECRET_KEY.getBytes());
+    
+}
