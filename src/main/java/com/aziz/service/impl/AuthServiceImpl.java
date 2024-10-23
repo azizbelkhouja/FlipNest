c185d1c1205bdf05d9b6b21e7a6219c2d820a4b3
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-10-23 18:28:48 +0200

    new user

--- src/main/java/com/aziz/service/impl/AuthServiceImpl.java
+++ src/main/java/com/aziz/service/impl/AuthServiceImpl.java
@@ -1,9 +1,12 @@
 package com.aziz.service.impl;
 
+import com.aziz.domain.USER_ROLE;
+import com.aziz.modal.User;
 import com.aziz.repository.UserRepository;
 import com.aziz.response.SignupRequest;
 import com.aziz.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,9 +14,23 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public String createUser(SignupRequest req) {
+
+        User user = userRepository.findByEmail(req.getEmail());
+
+        if (user == null) {
+            User  createdUser = new User();
+            createdUser.setEmail(req.getEmail());
+            createdUser.setFullName(req.getFullName());
+            createdUser.setRole(USER_ROLE.ROLE_CUSTOMER);
+            createdUser.setMobile("123456789");
+            createdUser.setPassword(passwordEncoder.encode(req.getOtp()));
+
+            user = userRepository.save(createdUser);
+        }
         return "";
     }
 }
