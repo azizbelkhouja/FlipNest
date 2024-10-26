035e84805d40c8f0424f6b5c97e3de45aeb83b4c
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-10-26 17:12:03 +0200

    impl done

--- src/main/java/com/aziz/service/impl/CustomUserServiceImpl.java
+++ src/main/java/com/aziz/service/impl/CustomUserServiceImpl.java
@@ -3,16 +3,20 @@ package com.aziz.service.impl;
 import com.aziz.domain.USER_ROLE;
 import com.aziz.modal.User;
 import com.aziz.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomUserServiceImpl implements UserDetailsService {
 
 
     private UserRepository userRepository;
     private static final String SELLER_PREFIX = "Seller_";
-
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -32,6 +36,16 @@ public class CustomUserServiceImpl implements UserDetailsService {
 
     private UserDetails buildUserDetails(String email, String password, USER_ROLE role) {
 
+        if(role == null) role = USER_ROLE.ROLE_CUSTOMER;
+
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        authorityList.add(new SimpleGrantedAuthority("ROLE_" + role));
+
+        return new org.springframework.security.core.userdetails.User (
+                email,
+                password,
+                authorityList
+        );
         
     }
 }
