ff0f057c9ac30c8f94f348aef5af531d44151344
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-10-26 18:21:20 +0200

    fix ||

--- src/main/java/com/aziz/service/impl/AuthServiceImpl.java
+++ src/main/java/com/aziz/service/impl/AuthServiceImpl.java
@@ -37,7 +37,7 @@ public class AuthServiceImpl implements AuthService {
 
         VerificationCode verificationCode = verificationCodeRepository.findByEmail(req.getEmail());
 
-        if (verificationCode == null && !verificationCode.getOtp().equals(req.getOtp())) {
+        if (verificationCode == null || !verificationCode.getOtp().equals(req.getOtp())) {
             throw new Exception("Wrong Otp");
         }
 
