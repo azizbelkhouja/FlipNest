f4499a4d44181d8e0b746142574bfd8637c5fbb4
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-10-27 14:30:51 +0100

    Login verification

--- src/main/java/com/aziz/request/LoginRequest.java
+++ src/main/java/com/aziz/request/LoginRequest.java
@@ -1,5 +1,8 @@
 package com.aziz.request;
 
+import lombok.Data;
+
+@Data
 public class LoginRequest {
 
     private String email;
--- src/main/java/com/aziz/service/impl/AuthServiceImpl.java
+++ src/main/java/com/aziz/service/impl/AuthServiceImpl.java
@@ -15,15 +15,18 @@ import com.aziz.service.AuthService;
 import com.aziz.service.EmailService;
 import com.aziz.utils.OtpUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
@@ -36,6 +39,7 @@ public class AuthServiceImpl implements AuthService {
     private final JwtProvider jwtProvider;
     private final VerificationCodeRepository verificationCodeRepository;
     private final EmailService emailService;
+    private final CustomUserServiceImpl customUserServiceImpl;
 
     @Override
     public void sendOtp(String email) throws Exception {
@@ -106,6 +110,41 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public AuthResponse signin(LoginRequest req) {
-        return null;
+
+        String username = req.getEmail();
+        String otp = req.getOtp();
+
+        Authentication authentication = authenticate(username, otp);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = jwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(token);
+        authResponse.setMessage("Logged in Successfully");
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        String roleName = authorities.isEmpty() ? null : authorities.iterator().next().getAuthority();
+
+        authResponse.setRole(USER_ROLE.valueOf(roleName));
+
+        return authResponse;
+    }
+
+    private Authentication authenticate(String username, String otp) {
+
+         UserDetails userDetails = customUserServiceImpl.loadUserByUsername(username);
+
+         if (userDetails == null) {
+             throw new BadCredentialsException("Invalid Username");
+         }
+
+         VerificationCode verificationCode = verificationCodeRepository.findByEmail(username);
+
+         if (verificationCode == null || !verificationCode.getOtp().equals(otp)) {
+             throw new BadCredentialsException("Wrong Verification Code");
+         }
+
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
 }
