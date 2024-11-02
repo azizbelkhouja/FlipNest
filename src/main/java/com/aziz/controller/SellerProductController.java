58b0d5f0f2e1f99eba956f9ecb793fddd28e5c11
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-02 18:04:31 +0100

    getProductBySellerId

--- src/main/java/com/aziz/controller/SellerProductController.java
+++ src/main/java/com/aziz/controller/SellerProductController.java
@@ -1,14 +1,38 @@
 package com.aziz.controller;
 
 
+import com.aziz.exceptions.ProductException;
+import com.aziz.exceptions.SellerException;
+import com.aziz.modal.Product;
+import com.aziz.modal.Seller;
+import com.aziz.service.ProductService;
+import com.aziz.service.SellerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/sellers")
+@RequestMapping("/api/seller/product")
 public class SellerProductController {
 
+    private final ProductService productService;
+    private final SellerService sellerService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getProductBySellerId(
+            @RequestHeader("Authorization") String jwt) throws Exception {
+
+        Seller seller = sellerService.getSellerProfile(jwt);
+
+        List<Product> products = productService.getProductBySellerId(seller.getId());
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
 
 }
