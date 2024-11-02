ff5b0cea1598f51f11ff79b7733eb72d7044aea6
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-02 17:53:18 +0100

    searchProduct

--- src/main/java/com/aziz/controller/ProductController.java
+++ src/main/java/com/aziz/controller/ProductController.java
@@ -6,10 +6,9 @@ import com.aziz.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,5 +25,13 @@ public class ProductController {
         Product product = productService.findProductById(productId);
         return new ResponseEntity<>(product, HttpStatus.OK);
     }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Product>> searchProduct(
+            @RequestParam(required = false) String query) {
+        List<Product> products = productService.searchProducts(query);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
 
 }
