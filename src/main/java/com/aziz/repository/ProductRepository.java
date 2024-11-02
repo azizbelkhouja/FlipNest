868906a39060d9bedd4b302160b581af5a94763e
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-02 17:26:55 +0100

    JpaSpecificationExecutor

--- src/main/java/com/aziz/repository/ProductRepository.java
+++ src/main/java/com/aziz/repository/ProductRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
-//    Page<Product> findAll(Specification<Product> spec, Pageable pageable);
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+
 }
