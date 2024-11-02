36600774f1ea8f538f6172b4969e3268c42c089c
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-02 16:37:30 +0100

    ProductServiceImpl

---
+++ src/main/java/com/aziz/service/impl/ProductServiceImpl.java
@@ -0,0 +1,114 @@
+package com.aziz.service.impl;
+
+import com.aziz.modal.Category;
+import com.aziz.modal.Product;
+import com.aziz.modal.Seller;
+import com.aziz.repository.CategoryRepository;
+import com.aziz.repository.ProductRepository;
+import com.aziz.request.CreateProductRequest;
+import com.aziz.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+
+    @Override
+    public Product createProduct(CreateProductRequest req, Seller seller) {
+
+        Category category1 = categoryRepository.findByCategoryId(req.getCategory());
+
+        if (category1 == null) {
+            Category category = new Category();
+            category.setCategoryId(req.getCategory());
+            category.setLevel(1);
+            category1 = categoryRepository.save(category);
+        }
+
+        Category category2 = categoryRepository.findByCategoryId(req.getCategory());
+
+        if (category2 == null) {
+            Category category = new Category();
+            category.setCategoryId(req.getCategory());
+            category.setLevel(2);
+            category.setParentCategory(category1);
+            category2 = categoryRepository.save(category);
+        }
+
+        Category category3 = categoryRepository.findByCategoryId(req.getCategory());
+
+        if (category3 == null) {
+            Category category = new Category();
+            category.setCategoryId(req.getCategory());
+            category.setLevel(3);
+            category.setParentCategory(category2);
+            category2 = categoryRepository.save(category);
+        }
+
+        int discountPercentage = calculateDiscountPercentage(req.getMrpPrice(), req.getSellingPrice());
+
+        Product product = new Product();
+        product.setSeller(seller);
+        product.setCategory(category3);
+        product.setDescription(req.getDescription());
+        product.setCreatedAt(LocalDateTime.now());
+        product.setTitle(req.getTitle());
+        product.setColor(req.getColor());
+        product.setSellingPrice(req.getSellingPrice());
+        product.setImages(req.getImages());
+        product.setMrpPrice(req.getMrpPrice());
+        product.setSizes(req.getSizes());
+        product.setDiscountPercent();
+
+        return productRepository.save(product);
+    }
+
+    private int calculateDiscountPercentage(int mrpPrice, int sellingPrice) {
+
+        if (mrpPrice <= 0) {
+            throw new IllegalArgumentException("Actual Price must be > 0");
+        }
+        double discount = mrpPrice - sellingPrice;
+        double discountPercentage = (discount/mrpPrice) * 100;
+
+    }
+
+    @Override
+    public void deleteProduct(Long productId) {
+
+    }
+
+    @Override
+    public Product updateProduct(Long productId, Product product) {
+        return null;
+    }
+
+    @Override
+    public Product findProductById(Long productId) {
+        return null;
+    }
+
+    @Override
+    public List<Product> searchProducts() {
+        return List.of();
+    }
+
+    @Override
+    public Page<Product> getAllProducts(String category, String brand, String colors, String sizes, Integer minPrice, Integer maxPrice, Integer minDiscount, String sort, String stock, Integer pageNumber) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getProductBySellerId(Long sellerId) {
+        return List.of();
+    }
+}
