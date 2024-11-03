eaa09d14447f3f22aee142a77faac60714a14680
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-03 23:13:35 +0100

    createOrder

--- src/main/java/com/aziz/service/impl/OrderServiceImpl.java
+++ src/main/java/com/aziz/service/impl/OrderServiceImpl.java
@@ -1,18 +1,63 @@
 package com.aziz.service.impl;
 
 import com.aziz.domain.OrderStatus;
-import com.aziz.modal.Address;
-import com.aziz.modal.Cart;
-import com.aziz.modal.Order;
-import com.aziz.modal.User;
+import com.aziz.domain.PaymentStatus;
+import com.aziz.modal.*;
+import com.aziz.repository.AddressRepository;
+import com.aziz.repository.OrderRepository;
 import com.aziz.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
+@Service
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+    private final AddressRepository addressRepository;
+
     @Override
     public Set<Order> createOrder(User user, Address shippingAddress, Cart cart) {
+
+        if (!user.getAddresses().contains(shippingAddress)) {
+            user.getAddresses().add(shippingAddress);
+        }
+        Address address = addressRepository.save(shippingAddress);
+
+        Map<Long, List<CartItem>> itemsBySeller = cart.getCartItems().stream()
+                .collect(Collectors.groupingBy(item->item.getProduct()
+                        .getSeller().getId()));
+        Set<Order> orders = new HashSet<>();
+
+        for (Map.Entry<Long, List<CartItem>> entry : itemsBySeller.entrySet()) {
+            Long sellerId = entry.getKey();
+            List<CartItem> items = entry.getValue();
+
+            int totalOrderPrice = items.stream().mapToInt(
+                    CartItem::getSellingPrice
+            ).sum();
+            int totalItem = items.stream().mapToInt(CartItem::getQuantity).sum();
+
+            Order createdOrder = new Order();
+            createdOrder.setUser(user);
+            createdOrder.setSellerId(sellerId);
+            createdOrder.setTotalMrpPrice(totalOrderPrice);
+            createdOrder.setTotalSellingPrice(totalOrderPrice);
+            createdOrder.setTotalItem(totalItem);
+            createdOrder.setShippingAddress(address);
+            createdOrder.setOrderStatus(OrderStatus.PENDING);
+            createdOrder.getPaymentDetails().setStatus(PaymentStatus.PENDING);
+
+            Order savedOrder = orderRepository.save(createdOrder);
+            orders.add(savedOrder);
+
+            List<OrderItem> OrderItems = new ArrayList<>();
+        }
+
+
         return Set.of();
     }
 
