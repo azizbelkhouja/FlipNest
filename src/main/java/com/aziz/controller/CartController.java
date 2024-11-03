3205e66545828be1f49d59eacc039dc12a7412a1
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-03 17:38:34 +0100

    findUserCartHandler

--- src/main/java/com/aziz/controller/CartController.java
+++ src/main/java/com/aziz/controller/CartController.java
@@ -1,9 +1,16 @@
 package com.aziz.controller;
 
 
+import com.aziz.modal.Cart;
+import com.aziz.modal.User;
 import com.aziz.service.CartItemService;
 import com.aziz.service.CartService;
+import com.aziz.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +21,17 @@ public class CartController {
 
     private final CartService cartService;
     private final CartItemService cartItemService;
+    private final UserService userService;
 
+    @GetMapping
+    public ResponseEntity<Cart> findUserCartHandler(@RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+        Cart cart = cartService.findUserCart(user);
+
+        System.out.println("cart - " + cart.getUser().getEmail());
+
+        return new ResponseEntity<Cart>(cart, HttpStatus.OK);
+    }
 
 
 
