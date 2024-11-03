6a553b48c1d909779d4df9b0eb67ebda2cdacecd
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-03 17:14:08 +0100

    CartItemService

--- src/main/java/com/aziz/service/CartItemService.java
+++ src/main/java/com/aziz/service/CartItemService.java
@@ -1,4 +1,9 @@
 package com.aziz.service;
 
+import com.aziz.modal.CartItem;
+
 public interface CartItemService {
+
+    CartItem updateCartItem(Long userId, Long id, CartItem cartItem);
+    void removeCartItem(Long userId, Long cartItemId);
 }
