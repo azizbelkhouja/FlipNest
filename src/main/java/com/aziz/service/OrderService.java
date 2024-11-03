341d966fbf799e84b25bbe844fef7a8ec0670833
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-03 22:02:02 +0100

    OrderService

---
+++ src/main/java/com/aziz/service/OrderService.java
@@ -0,0 +1,19 @@
+package com.aziz.service;
+
+import com.aziz.domain.OrderStatus;
+import com.aziz.modal.Address;
+import com.aziz.modal.Cart;
+import com.aziz.modal.Order;
+import com.aziz.modal.User;
+
+import java.util.List;
+import java.util.Set;
+
+public interface OrderService {
+
+    Set<Order> createOrder(User user, Address shippingAddress, Cart cart);
+    Order findOrderById(Long id);
+    List<Order> userOrdersHistory(Long userId);
+    List<Order> sellerOrders(Long SellerId);
+    Order updateOrderStatus(Long id, OrderStatus orderStatus);
+}
