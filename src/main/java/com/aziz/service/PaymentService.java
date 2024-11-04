aec96188be3f6b735b30c4c9218f7ae8b573dbdc
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-04 21:19:11 +0100

    createStripePaymentLink

--- src/main/java/com/aziz/service/PaymentService.java
+++ src/main/java/com/aziz/service/PaymentService.java
@@ -3,6 +3,7 @@ package com.aziz.service;
 import com.aziz.modal.Order;
 import com.aziz.modal.PaymentOrder;
 import com.aziz.modal.User;
+import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentLink;
 
 import java.util.Set;
@@ -12,6 +13,5 @@ public interface PaymentService {
     PaymentOrder createOrder(User user, Set<Order> orders);
     PaymentOrder getPaymentOrderById(Long orderId) throws Exception;
     PaymentOrder getPaymentOrderByPaymentId(String paymentId) throws Exception;
-    Boolean ProceedPaymentOrder(PaymentOrder paymentOrder, String paymentId, String paymentLinkId);
-    String createStripePaymentLink(User user, Long amount, Long orderId);
+    String createStripePaymentLink(User user, Long amount, Long orderId) throws StripeException;
 }
--- src/main/java/com/aziz/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/aziz/service/impl/PaymentServiceImpl.java
@@ -1,11 +1,19 @@
 package com.aziz.service.impl;
 
+import com.aziz.domain.PaymentOrderStatus;
 import com.aziz.modal.Order;
 import com.aziz.modal.PaymentOrder;
 import com.aziz.modal.User;
 import com.aziz.repository.OrderRepository;
 import com.aziz.repository.PaymentOrderRepository;
 import com.aziz.service.PaymentService;
+import com.stripe.Stripe;
+import com.stripe.StripeClient;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.PaymentIntentConfirmParams;
+import com.stripe.param.checkout.SessionCreateParams;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +25,8 @@ public class PaymentServiceImpl implements PaymentService {
 
     private PaymentOrderRepository paymentOrderRepository;
     private OrderRepository orderRepository;
+
+    private String stripeSecretKey ="stripesecretkey";
 
 
     @Override
@@ -53,12 +63,27 @@ public class PaymentServiceImpl implements PaymentService {
     }
 
     @Override
-    public Boolean ProceedPaymentOrder(PaymentOrder paymentOrder, String paymentId, String paymentLinkId) {
-        return null;
-    }
+    public String createStripePaymentLink(User user, Long amount, Long orderId) throws StripeException {
 
-    @Override
-    public String createStripePaymentLink(User user, Long amount, Long orderId) {
+        Stripe.apiKey = stripeSecretKey;
+        SessionCreateParams params = SessionCreateParams.builder().addPaymentMethodType(SessionCreateParams
+                .PaymentMethodType.CARD)
+                .setMode(SessionCreateParams.Mode.PAYMENT)
+                .setSuccessUrl("http://localhost3000/payment-success/" + orderId)
+                .setCancelUrl("http://localhost3000/payment-cancelled/" + orderId)
+                .addLineItem(SessionCreateParams.LineItem.builder()
+                        .setQuantity(1L)
+                        .setPriceData(SessionCreateParams.LineItem.PriceData.builder()
+                                .setCurrency("eur")
+                                .setUnitAmount(amount * 100)
+                                .setProductData(SessionCreateParams.LineItem.PriceData.ProductData
+                                        .builder().setName("Aziz Payment").build()
+                                ).build()
+                        ).build()
+                ).build();
+
+        Session session = Session.create(params);
+
         return "";
     }
 }
