5262ce6c3616156642298ac03c36e3f2c3deca2a
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-04 14:40:07 +0100

    usersOrderHistoryHandler

---
+++ src/main/java/com/aziz/controller/OrderController.java
@@ -0,0 +1,72 @@
+package com.aziz.controller;
+
+
+import com.aziz.domain.PaymentMethod;
+import com.aziz.modal.*;
+import com.aziz.response.PaymentLinkResponse;
+import com.aziz.service.CartService;
+import com.aziz.service.OrderService;
+import com.aziz.service.UserService;
+import jdk.jshell.spi.ExecutionControl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+    private final UserService userService;
+    private final CartService cartService;
+
+    @PostMapping()
+    public ResponseEntity<PaymentLinkResponse> createOrderHandler(
+            @RequestBody Address shippingAddress,
+            @RequestParam PaymentMethod paymentMethod,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+        Cart cart = cartService.findUserCart(user);
+        Set<Order> orders = orderService.createOrder(user, shippingAddress, cart);
+
+//        PaymentOrder paymentOrder = paymentService.createOrder(user, orders);
+
+        PaymentLinkResponse res = new PaymentLinkResponse();
+
+//        if (paymentMethod.equals(PaymentMethod.PAYPAL)) {
+//            PaymentLink payment = paymentService.createRazorpayPaymentLink(
+//                    user,
+//                    paymentOrder.getAmount(),
+//                    paymentOrder.getId()
+//            );
+//            String paymentUrl = payment.get("short_url");
+//            String paymentUrlId = payment.get("id");
+//
+//            res.setPayment_link_url(paymentUrl);
+//
+//            paymentOrder.setPaymentLinkId(paymentUrlId);
+//            paymentOrderRepository.save(paymentOrder);
+//        }
+
+        return new ResponseEntity<>(res, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<List<Order>> usersOrderHistoryHandler(
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+        List<Order> orders = orderService.userOrdersHistory(user.getId());
+        return new ResponseEntity<>(orders, HttpStatus.ACCEPTED);
+    }
+
+
+
+}
