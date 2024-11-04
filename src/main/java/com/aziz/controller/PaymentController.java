0310f0d13c2cb0e1e474a7f5806fbaf257323ea8
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-04 21:21:16 +0100

    PaymentController

---
+++ src/main/java/com/aziz/controller/PaymentController.java
@@ -0,0 +1,13 @@
+package com.aziz.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/payment")
+public class PaymentController {
+
+
+}
