0af5bdef1d76d423dfb3bc6fa2974634ed8ed905
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-08 18:32:05 +0100

    createTransaction

---
+++ src/main/java/com/aziz/service/impl/TransactionServiceImpl.java
@@ -0,0 +1,41 @@
+package com.aziz.service.impl;
+
+import com.aziz.modal.Order;
+import com.aziz.modal.Seller;
+import com.aziz.modal.Transaction;
+import com.aziz.repository.SellerRepository;
+import com.aziz.repository.TransactionRepository;
+import com.aziz.service.TransactionService;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class TransactionServiceImpl implements TransactionService{
+
+    private final TransactionRepository transactionRepository;
+    private final SellerRepository sellerRepository;
+
+    @Override
+    public Transaction createTransaction(Order order) {
+
+        Seller seller = sellerRepository.findById(order.getSellerId()).get();
+
+        Transaction transaction = new Transaction();
+        transaction.setSeller(seller);
+        transaction.setCustomer(order.getUser());
+        transaction.setOrder(order);
+
+        return transactionRepository.save(transaction);
+    }
+
+    @Override
+    public List<Transaction> getTransactionBySellerId(Seller seller) {
+        return List.of();
+    }
+
+    @Override
+    public List<Transaction> getAllTransactions() {
+        return List.of();
+    }
+}
