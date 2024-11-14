4396bed3b104cb6867a90ff55d211e792b492d2c
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-14 18:22:23 +0100

    applyCoupon

--- src/main/java/com/aziz/service/impl/CouponServiceImpl.java
+++ src/main/java/com/aziz/service/impl/CouponServiceImpl.java
@@ -3,6 +3,7 @@ package com.aziz.service.impl;
 import com.aziz.modal.Cart;
 import com.aziz.modal.Coupon;
 import com.aziz.modal.User;
+import com.aziz.repository.CartRepository;
 import com.aziz.repository.CouponRepository;
 import com.aziz.service.CouponService;
 import lombok.RequiredArgsConstructor;
@@ -15,12 +16,24 @@ import java.util.List;
 public class CouponServiceImpl implements CouponService {
 
     private final CouponRepository couponRepository;
+    private final CartRepository cartRepository;
 
 
     @Override
-    public Cart applyCoupon(String code, double orderValue, User user) {
+    public Cart applyCoupon(String code, double orderValue, User user) throws Exception {
 
+        Coupon coupon = couponRepository.findByCode(code);
+        Cart cart = cartRepository.findByUserId(user.getId());
 
+        if (coupon == null) {
+            throw new Exception("coupon not valid");
+        }
+        if (user.getUsedCoupons().contains(coupon)) {
+            throw new Exception("coupon already used");
+        }
+        if (orderValue < coupon.getMinimumOrderValue()) {
+            throw new Exception("Add more orders to use coupon");
+        }
 
         return null;
     }
