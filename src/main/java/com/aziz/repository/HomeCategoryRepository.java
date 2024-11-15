d6ff61ff6fab4957663e72c2a2b55d0871785f40
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-15 17:05:24 +0100

    HomeCategoryService

---
+++ src/main/java/com/aziz/repository/HomeCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.aziz.repository;
+
+import com.aziz.modal.HomeCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HomeCategoryRepository extends JpaRepository<HomeCategory, Long> {
+
+
+}
---
+++ src/main/java/com/aziz/service/HomeCategoryService.java
@@ -0,0 +1,14 @@
+package com.aziz.service;
+
+import com.aziz.modal.HomeCategory;
+
+import java.util.List;
+
+public interface HomeCategoryService {
+
+    HomeCategory createHomeCategory(HomeCategory homeCategory);
+    List<HomeCategory> createCategories(List<HomeCategory> homeCategories);
+    HomeCategory updateHomeCategory(HomeCategory homeCategory, Long id);
+    List<HomeCategory> getAllHomeCategories();
+
+}
