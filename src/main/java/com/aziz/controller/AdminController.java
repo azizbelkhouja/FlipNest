bb5ec51476ac6c42c0466d235adc2e06ac2160d7
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-15 17:42:09 +0100

    AdminController
    updateSellerStatus

---
+++ src/main/java/com/aziz/controller/AdminController.java
@@ -0,0 +1,28 @@
+package com.aziz.controller;
+
+import com.aziz.domain.AccountStatus;
+import com.aziz.exceptions.SellerException;
+import com.aziz.modal.Seller;
+import com.aziz.service.SellerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final SellerService sellerService;
+
+    @PatchMapping("/seller/{id}/status/{status}")
+    public ResponseEntity<Seller> updateSellerStatus(
+            @PathVariable Long id,
+            @PathVariable AccountStatus status) throws Exception {
+
+        Seller updatedSeller = sellerService.updateSellerAccountStatus(id, status);
+        return ResponseEntity.ok(updatedSeller);
+    }
+
+}
--- src/main/java/com/aziz/controller/HomeCategoryController.java
+++ src/main/java/com/aziz/controller/HomeCategoryController.java
@@ -7,9 +7,7 @@ import com.aziz.service.HomeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,7 +28,18 @@ public class HomeCategoryController {
         return new ResponseEntity<>(home, HttpStatus.ACCEPTED);
     }
 
+    @GetMapping("/admin/home-category")
+    public ResponseEntity<List<HomeCategory>> getHomeCategory() throws Exception {
+        List<HomeCategory> categories = homeCategoryService.getAllHomeCategories();
+        return ResponseEntity.ok(categories);
+    }
 
-
+    @PatchMapping("/admin/home-category/{id}")
+    public ResponseEntity<HomeCategory> updateHomeCategory(
+            @PathVariable Long id,
+            @RequestBody HomeCategory homeCategory) throws Exception {
+        HomeCategory updatedCategory = homeCategoryService.updateHomeCategory(homeCategory, id);
+        return ResponseEntity.ok(updatedCategory);
+    }
 
 }
