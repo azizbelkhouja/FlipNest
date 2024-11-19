662ee2f52b6d1623e2eebaaaab91ecfd308c6e84
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-19 13:53:09 +0100

    customTheme

---
+++ frontend/src/theme/customTheme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+const customTheme = createTheme({
+    palette:{
+        mode:"light",
+        primary:{
+            main: "#048690"
+        },
+        secondary:{
+            main: "#77BDC3"
+        }
+    }
+
+})
+
+export default customTheme;
