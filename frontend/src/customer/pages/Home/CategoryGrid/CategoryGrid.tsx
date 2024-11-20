717e78e353f525bf2247ad9badc157fd59c22dcd
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-20 13:25:52 +0100

    Deal of the day

--- frontend/src/customer/pages/Home/CategoryGrid/CategoryGrid.tsx
+++ frontend/src/customer/pages/Home/CategoryGrid/CategoryGrid.tsx
@@ -4,7 +4,22 @@ const CategoryGrid = () => {
   return (
     <div className='grid gap-4 grid-rows-12 grid-cols-12 lg:h-[600px] px-5 lg:px-20'>
         <div className='col-span-3 row-span-12 text-white'>
-            <img src="https://images.pexels.com/photos/2826131/pexels-photo-2826131.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            <img className='w-full h-full object-fill object-top rounded-md' src="https://images.pexels.com/photos/2826131/pexels-photo-2826131.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        </div>
+        <div className='col-span-2 row-span-6 text-white'>
+            <img className='w-full h-full object-fill object-top rounded-md' src="https://images.pexels.com/photos/4069290/pexels-photo-4069290.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        </div>
+        <div className='col-span-4 row-span-6 text-white'>
+            <img className='w-full h-full object-fill object-top rounded-md' src="https://images.pexels.com/photos/1462630/pexels-photo-1462630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        </div>
+        <div className='col-span-3 row-span-12 text-white'>
+            <img className='w-full h-full object-fill object-top rounded-md' src="https://images.pexels.com/photos/1848731/pexels-photo-1848731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        </div>
+        <div className='col-span-4 row-span-6 text-white'>
+            <img className='w-full h-full object-fill object-top rounded-md' src="https://images.pexels.com/photos/4145197/pexels-photo-4145197.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        </div>
+        <div className='col-span-2 row-span-6 text-white'>
+            <img className='w-full h-full object-fill object-top rounded-md' src="https://images.pexels.com/photos/3414792/pexels-photo-3414792.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
         </div>
     </div>
   )
--- frontend/src/customer/pages/Home/Home.tsx
+++ frontend/src/customer/pages/Home/Home.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import ElectronicsCategory from './ElectronicsCategory/ElectronicsCategory'
 import CategoryGrid from './CategoryGrid/CategoryGrid'
+import Deal from './Deal/Deal'
 
 const Home = () => {
   return (
     <>
 
-    <div className='space-y-5 lg:space-y-10 relative'>
+    <div className='space-y-5 lg:space-y-10 relative pb-20'>
 
         <ElectronicsCategory/>
         <CategoryGrid/>
+        <Deal/>
 
     </div>
     
