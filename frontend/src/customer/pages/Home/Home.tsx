687bb73efafa7ca15e00802a357c467e9fd1b2cc
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-20 14:42:22 +0100

    Home
    header image

--- frontend/src/customer/pages/Home/Home.tsx
+++ frontend/src/customer/pages/Home/Home.tsx
@@ -12,12 +12,23 @@ const Home = () => {
 
         <ElectronicsCategory/>
         <CategoryGrid/>
-        <Deal/>
 
-        <div>
-          <h1 className='text-lg lg:text-4xl font-bold pb-5 lg:pb-20'>SHOP BY CATEGORY</h1>
-          <ShopByCategory/>
+        <div className='pt-20'>
+          <h1 className='text-lg lg:text-4xl font-bold pb-5 lg:pb-20 text-center'>TODAY'S TOP DEALS</h1>
+        <Deal/>
         </div>
+
+        <section className='pt-20'>
+          <h1 className='text-lg lg:text-4xl font-bold pb-5 lg:pb-20 text-center'>SHOP BY CATEGORY</h1>
+          <ShopByCategory/>
+        </section>
+
+        <section className='lg:px-20 relative h-[200px] lg:h-[450px] object-cover'>
+          <img className='w-full h-full' src="https://images.pexels.com/photos/4458554/pexels-photo-4458554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        </section>
+
+
+
 
     </div>
     
