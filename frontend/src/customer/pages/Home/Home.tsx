d208f860f40e52338c53425133ab04539c2fda76
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-20 15:15:51 +0100

    Home
    call to action footer

--- frontend/src/customer/pages/Home/Home.tsx
+++ frontend/src/customer/pages/Home/Home.tsx
@@ -3,6 +3,8 @@ import ElectronicsCategory from './ElectronicsCategory/ElectronicsCategory'
 import CategoryGrid from './CategoryGrid/CategoryGrid'
 import Deal from './Deal/Deal'
 import ShopByCategory from './ShopByCategory/ShopByCategory'
+import { Button } from '@mui/material'
+import StorefrontIcon from '@mui/icons-material/Storefront';
 
 const Home = () => {
   return (
@@ -18,13 +20,38 @@ const Home = () => {
         <Deal/>
         </div>
 
-        <section className='pt-20'>
+        <section className='py-20'>
           <h1 className='text-lg lg:text-4xl font-bold pb-5 lg:pb-20 text-center'>SHOP BY CATEGORY</h1>
           <ShopByCategory/>
         </section>
 
         <section className='lg:px-20 relative h-[200px] lg:h-[450px] object-cover'>
-          <img className='w-full h-full' src="https://images.pexels.com/photos/4458554/pexels-photo-4458554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          <img className='w-full h-full' src="https://images.pexels.com/photos/24740473/pexels-photo-24740473/free-photo-of-smiling-woman-lying-down-with-laptop-in-bed.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          <div className='absolute top-1/2 left-4 lg:left-[15rem] transform -translate-y-1/2 font-semibold lg:text-4xl space-y-3'>
+            <h1>Sell Your Product</h1>
+            <p className='text-lg md:text-2xl'>On <span className='logo'>FlipNest</span></p>
+            <div className='pt-6 flex justify-center'>
+              <Button 
+                startIcon={<StorefrontIcon />} 
+                variant="contained" 
+                sx={{
+                  color: 'white', 
+                  borderColor: 'white', 
+                  backgroundColor: '#77BDC3',
+                  '&:hover': {
+                    borderColor: 'white',
+                    backgroundColor: '#048690',
+                  },
+                  '&:active': {
+                    transform: 'scale(0.97)',
+                  },
+                }}
+              >
+                Become a Seller Now
+              </Button>
+
+            </div>
+          </div>
         </section>
 
 
