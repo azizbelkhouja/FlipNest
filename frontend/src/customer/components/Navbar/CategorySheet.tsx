a7a2c71642f85b24f4bf83b5e6d9d4fa48c66f2c
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-21 00:08:49 +0100

    CategorySheet
    level 2 categories

--- frontend/src/customer/components/Navbar/CategorySheet.tsx
+++ frontend/src/customer/components/Navbar/CategorySheet.tsx
@@ -3,6 +3,11 @@ import { studyresourcesleveltwo } from '../../data/category/level-two/studyresou
 import { electronicsleveltwo } from '../../data/category/level-two/electronicsleveltwo'
 import { clothingleveltwo } from '../../data/category/level-two/clothingleveltwo'
 import { furnituresleveltwo } from '../../data/category/level-two/furnituresleveltwo'
+import { studyResourcesItems } from '../../data/category/level-three/studyresourceslevelthree'
+import { electronicsItems } from '../../data/category/level-three/electronicslevelthree'
+import { clothingItems } from '../../data/category/level-three/clothinglevelthree'
+import { furnitureAndDormItems } from '../../data/category/level-three/furnitureslevelthree'
+import { Box } from '@mui/material'
 
 const categoryTwo = {
     resources: studyresourcesleveltwo,
@@ -11,12 +16,24 @@ const categoryTwo = {
     furnitures: furnituresleveltwo
 }
 const categoryThree = {
-    
+    resources: studyResourcesItems,
+    electronics: electronicsItems,
+    clothing: clothingItems,
+    furnitures: furnitureAndDormItems
 }
 
 const CategorySheet = () => {
   return (
-    <div>CategorySheet</div>
+    <Box className='bg-white shadow-lg lh:h-[500px] overflow-y-auto'>
+        <div className='flex text-sm flex-wrap'>
+            {
+                categoryTwo["resources"].map((item) => 
+                    <div>
+                        <p className='text-primaryblue mb-5 font-semibold'></p>
+                    </div>)
+            }
+        </div>
+    </Box>
   )
 }
 
