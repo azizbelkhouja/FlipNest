cecaf6a46c752df4eeb2df578e4fe7f0bbf9800b
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-21 00:01:20 +0100

    CategorySheet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './customer/components/Navbar';
+import Navbar from './customer/components/Navbar/Navbar';
 import Home from './customer/pages/Home/Home';
 
 
---
+++ frontend/src/customer/components/Navbar/CategorySheet.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { studyresourcesleveltwo } from '../../data/category/level-two/studyresourcesleveltwo'
+import { electronicsleveltwo } from '../../data/category/level-two/electronicsleveltwo'
+import { clothingleveltwo } from '../../data/category/level-two/clothingleveltwo'
+import { furnituresleveltwo } from '../../data/category/level-two/furnituresleveltwo'
+
+const categoryTwo = {
+    resources: studyresourcesleveltwo,
+    electronics: electronicsleveltwo,
+    clothing: clothingleveltwo,
+    furnitures: furnituresleveltwo
+}
+const categoryThree = {
+    
+}
+
+const CategorySheet = () => {
+  return (
+    <div>CategorySheet</div>
+  )
+}
+
+export default CategorySheet
