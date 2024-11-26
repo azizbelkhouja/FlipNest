c21139f695ba2e312bdb89aeb2ec965971da21d3
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-26 15:36:37 +0100

    fix in Electronic level 2 category

--- frontend/src/customer/data/category/level-two/electronicsleveltwo.ts
+++ frontend/src/customer/data/category/level-two/electronicsleveltwo.ts
@@ -8,16 +8,19 @@ export const electronicsleveltwo = [
     {
       name: "Mobile Phones",
       categoryId: "Mobile Phones",
+      parentCategoryId: "Electronics",
       level: 2,
     },
     {
       name: "Gaming Consoles",
       categoryId: "Gaming Consoles",
+      parentCategoryId: "Electronics",
       level: 2,
     },
     {
       name: "Accessories",
       categoryId: "Accessories",
+      parentCategoryId: "Electronics",
       level: 2,
     },
   ];
