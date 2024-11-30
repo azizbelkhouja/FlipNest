a14abce7e1696aeacf016ff2d48f0c32bfe34821
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-11-30 23:09:31 +0100

    ProductCard

--- frontend/src/customer/pages/Product/ProductCard.tsx
+++ frontend/src/customer/pages/Product/ProductCard.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
+const images = [
+  "",
+  ""
+]
+
 const ProductCard = () => {
   return (
-    <div>ProductCard</div>
+    <>
+      <div className='group px-4 relative'>
+        <div className='card'>
+          {images.map((item) => <img className='card-media object-top' src="" alt="" />)}
+          
+        </div>
+      </div>
+    </>
   )
 }
 
