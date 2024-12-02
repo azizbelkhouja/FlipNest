2a1e3e15eb02b4afc17871b31c27e224e7c2d17d
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-12-02 22:48:39 +0100

    react router dom updates

--- frontend/src/customer/pages/Product/FilterSection.tsx
+++ frontend/src/customer/pages/Product/FilterSection.tsx
@@ -37,7 +37,7 @@ const FilterSection = () => {
     <div className='-z-50 space-y-5 bg-white'>
       <div className='flex items-center justify-between h-[40px] px-9 lg:border-r'>
         <p className="text-lg font-semibold">Filters</p>
-        <Button size='small' className='text-primaryblue cursor-pointer font-semibold'>Clear All</Button>
+        <Button onClick={clearAllFilters} size='small' className='text-primaryblue cursor-pointer font-semibold'>Clear All</Button>
       </div>
       <Divider/>
       <div className='px-9 space-y-6'>
