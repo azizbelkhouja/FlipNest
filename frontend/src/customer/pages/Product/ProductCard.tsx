fde798c0aa1072145d31c68e5474ffa2b3fd33b2
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-12-02 13:32:19 +0100

    product card css

--- frontend/src/customer/pages/Product/ProductCard.tsx
+++ frontend/src/customer/pages/Product/ProductCard.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
 const images = [
-  "",
-  ""
+  "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MA7F4?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1723162550519",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK0X6vsKIE0WfiAp4Hsmc6iIF0YpN-R2WPLA&s"
 ]
 
 const ProductCard = () => {
   return (
     <>
       <div className='group px-4 relative'>
+
         <div className='card'>
           {images.map((item) => <img className='card-media object-top' src={item} alt="" />)}
-          
         </div>
+        
       </div>
     </>
   )
