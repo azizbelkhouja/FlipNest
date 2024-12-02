d2450c17e3a71b88e5560ba8fefede72a868a74d
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-12-02 14:49:56 +0100

    FilterSection
    header buttons

--- frontend/src/customer/pages/Product/FilterSection.tsx
+++ frontend/src/customer/pages/Product/FilterSection.tsx
@@ -1,8 +1,15 @@
+import { Button } from '@mui/material'
 import React from 'react'
 
 const FilterSection = () => {
   return (
-    <div>FilterSection</div>
+    <div className='-z-50 space-y-5 bg-white'>
+      <div className='flex items-center justify-between h-[40px] px-9 lg:border-r'>
+        <p className="text-lg font-semibold">Filters</p>
+        <Button size='small' className='text-primaryblue cursor-pointer font-semibold'>Clear All</Button>
+
+      </div>
+    </div>
   )
 }
 
