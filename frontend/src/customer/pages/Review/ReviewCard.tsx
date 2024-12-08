3ddbb99651af373948b4d978ec4c0696ab80cfae
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-12-08 17:51:38 +0100

    ReviewCard

--- frontend/src/customer/pages/Review/ReviewCard.tsx
+++ frontend/src/customer/pages/Review/ReviewCard.tsx
@@ -1,8 +1,19 @@
+import { Avatar, Box, Grid2 } from '@mui/material'
 import React from 'react'
 
 const ReviewCard = () => {
   return (
-    <div>ReviewCard</div>
+    <div className='flex justify-between'>
+      <Grid2 container spacing={2} gap={3}>
+        <Grid2 item xs={1}>
+          <Box>
+            <Avatar className='text-white' sx={{width:56, height:56, bgcolor: "#9155FD"}}>
+
+            </Avatar>
+          </Box>
+        </Grid2>
+      </Grid2>
+    </div>
   )
 }
 
