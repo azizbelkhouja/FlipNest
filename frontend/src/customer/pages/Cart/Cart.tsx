07434c9a43f5831942922842374a8feff616cd37
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-12-11 23:32:42 +0100

    started Cart 2

--- frontend/src/customer/pages/Cart/Cart.tsx
+++ frontend/src/customer/pages/Cart/Cart.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <div className='pt-10 px-5 sm:px-10 md:px-60 min-h-screen'>
+      <div>
+        
+      </div>
+    </div>
   )
 }
 
