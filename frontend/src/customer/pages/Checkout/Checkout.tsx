059f686b6625d060cbee1c1db5e96256037748b3
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-12-28 22:52:50 +0100

    modal open

--- frontend/src/customer/pages/Checkout/Checkout.tsx
+++ frontend/src/customer/pages/Checkout/Checkout.tsx
@@ -1,32 +1,69 @@
-import { Button } from '@mui/material'
+import { Box, Button, Modal, Typography } from '@mui/material'
 import React from 'react'
 import AddressCard from './AddressCard'
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+};
+  
+    
 const Checkout = () => {
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
   return (
-    <div className='pt-10 px-5 sm:px-10 md:px-44 lg:px-60 min-h-screen'>
-        <div className="space-y-5 lg:space-y-0 lg:grid grid-cols-3 lg:gap-9">
-            <div className="col-span-2 space-y-5">
+    <>
+        <div className='pt-10 px-5 sm:px-10 md:px-44 lg:px-60 min-h-screen'>
+            <div className="space-y-5 lg:space-y-0 lg:grid grid-cols-3 lg:gap-9">
+                <div className="col-span-2 space-y-5">
 
-                <div className="flex justify-between items-center">
-                    <h1 className="font-semibold">Select Shipping Address</h1>
-                    <Button>Add New Address</Button>
-                </div>
-
-                <div className="text-xs font-medium space-y-5">
-                    <p>Saved Addresses</p>
-                    <div className="space-y-3">
-                        {[1,1,1].map((item) => <AddressCard />)}
+                    <div className="flex justify-between items-center">
+                        <h1 className="font-semibold">Select Shipping Address</h1>
+                        <Button onClick={handleOpen}>Add New Address</Button>
                     </div>
-                </div>
 
-                <div className="py-4 px-5 rounded-md border">
-                    <Button>Add New Address</Button>
-                </div>
+                    <div className="text-xs font-medium space-y-5">
+                        <p>Saved Addresses</p>
+                        <div className="space-y-3">
+                            {[1,1,1].map((item) => <AddressCard />)}
+                        </div>
+                    </div>
 
+                    <div className="py-4 px-5 rounded-md border">
+                        <Button onClick={handleOpen}>Add New Address</Button>
+                    </div>
+
+                </div>
             </div>
         </div>
-    </div>
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        >
+        <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+        </Box>
+        </Modal>
+    
+    </>
+
+
   )
 }
 
